d7fadf5d5a0d2afc8818dbc16efee47abc7eb37a
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-16 10:35:13 +0300

    3. Data

--- AJAX/Program.cs
+++ AJAX/Program.cs
@@ -17,6 +17,7 @@ namespace AJAX
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetSection("ConnectionString:DbConn").Value));
 
             var app = builder.Build();
 
