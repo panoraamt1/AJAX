c0202107907141d08c76e77765a1b2afe598c93f
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-16 09:50:48 +0300

    1. Country

--- AJAX/Models/Country.cs
+++ AJAX/Models/Country.cs
@@ -2,5 +2,18 @@
 {
     public class Country
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string Code { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Name { get; set; }
+
+        [MaxLength(75)]
+        public string CurrencyName { get; set; }
     }
 }
--- AJAX/Program.cs
+++ AJAX/Program.cs
@@ -1,3 +1,12 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.ComponentModel;
+global using Microsoft.EntityFrameworkCore;
+global using AJAX.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using AJAX.Data;
+
 namespace AJAX
 {
     public class Program
