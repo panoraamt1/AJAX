623caf7c99b5572c2c885fb74ff035afe4aa98e1
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-24 21:26:26 +0300

    1. Start

---
+++ AjaxCustomerCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjaxCustomerCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
