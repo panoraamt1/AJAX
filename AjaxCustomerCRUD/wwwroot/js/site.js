629135b4b8ae707db717d16727dc2ea5079732b6
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-28 13:06:37 +0300

    11. Customer GetCities

--- AjaxCustomerCRUD/wwwroot/js/site.js
+++ AjaxCustomerCRUD/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function FillCities(lstCountryCtrl, lstCityId) {
+    var lstCities = $("#" + lstCityId);
+    lstCities.empty();
+    lstCities.append($('<option/>',
+        {
+            value: null,
+            text: "Select City"
+        }));
+    var selectedContry = lstCountryCtrl.options[lstCountryCtrl.selectedIndex].value;
+    if (selectedContry != null && selectedContry != '') {
+        $.getJSON('/Customer/getcitiesbycountry', { CountryId: selectedcountry }, function (cities) {
+            if (cities != null && !jQuery.isEmptyObject(cities)) {
+                $.each(cities, function (index, city) {
+                    lstCities.append($('<option/>',
+                        {
+                            value: city.value,
+                            text: city.text
+                        }));
+                });
+            };
+        });
+    }
+    return;
+}
+
+$(".custom-file-input").on("change", function () {
+    var fileName = $(this).val().split("\\").pop();
+    document.getElementById('PreviewPhoto').src = window.URL.createObjectURL(this.files[0]);
+    document.getElementById('PhotoUrl').value = fileName;
+});
