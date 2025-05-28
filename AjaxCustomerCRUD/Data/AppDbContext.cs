b688bf939de6df15a3fe901edc6501152a05413d
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-05-28 12:16:04 +0300

    7. Customer, Controller & Views #1

--- AjaxCustomerCRUD/Data/AppDbContext.cs
+++ AjaxCustomerCRUD/Data/AppDbContext.cs
@@ -15,5 +15,6 @@ namespace AjaxCustomerCRUD.Data
         }
         public virtual DbSet<City> Cities { get; set; }
         public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
     }
 }
